a67d8eb19c809358ae5801155116be2d4a04fb55
bozsikbeatrix <91243617+bozsikbeatrix@users.noreply.github.com>
2021-12-26 12:35:16 +0200

    elso n szam osszege indirekt rek fgv

---
+++ 2021_12_21_lab_16/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int paratlan(int n);
+
+int paros(int n){
+if(n==0){return 0;}
+return n+paratlan(n-1);
+}
+
+int paratlan(int n){
+if(n==1){return 1;}
+return n+paros(n-1);
+}
+
+int main()
+{
+    cout << paros(4);
+    return 0;
+}
