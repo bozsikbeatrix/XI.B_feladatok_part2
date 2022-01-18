0792e8e06e31d4591fec7582a2c1453e376d3bc0
bozsikbeatrix <91243617+bozsikbeatrix@users.noreply.github.com>
2022-01-18 09:24:52 +0200

    struct file es tomb

---
+++ lab_20_max_1/main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+struct diak{
+int azonosito;
+double media;
+string osztaly;
+string nev;
+}diakok[1000];
+
+int main()
+{
+    int max, idx;
+    int n;
+    ifstream input("input.txt");
+    input >> n;
+    for(int i=0; i<n; i++){
+        input >> diakok[i].azonosito;
+        input >> diakok[i].nev;
+        input >> diakok[i].osztaly;
+        input >> diakok[i].media;
+        if(i==0){
+            max=diakok[i].media;
+        }
+        if(diakok[i].media > max){
+            max= diakok[i].media;
+            idx= diakok[i].azonosito;
+        }
+    }
+    cout << idx << endl;
+    return 0;
+}
